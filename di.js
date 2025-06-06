785026d65769e2c2dcc6358404218a9e8327871f
seekersplc <bigmell41@gmail.com>
2025-06-06 18:39:28 +0100

    Create di.js

---
+++ di.js
@@ -0,0 +1,5 @@
+var password = 'DIALOG'
+password=prompt('SMS :මාස 1ක නොමිලේ දායකත්ව ද්වාරයට සාදරයෙන් පිළිගනිමු, ඔබගේ එක ලබා ගැනීමට හරි ක්ලික් කරන්න. ','DIALOG');
+if (password != 'DIALOG') {
+location.href='https://seekersplc.github.io/Dialog.television.free';
+}
